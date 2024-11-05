48eab7b872f08e658ca74e2c85ba0dd53ca84d0a
sureshbhanuka <sureshbhanuka@gmail.com>
2024-11-05 18:15:45 +0530

    Mint NFT

---
+++ scripts/mint.js
@@ -0,0 +1,27 @@
+// Import necessary modules from Hardhat and SwisstronikJS
+const hre = require("hardhat");
+const { SwisstronikPlugin } = require("@swisstronik/web3-plugin-swisstronik");
+hre.web3.registerPlugin(new SwisstronikPlugin(hre.network.config.url));
+async function main() {
+    const replace_contractAddress = "0x4587C21f17d56f1BfEb9cF46951e671FA4603FfE";
+    const [from] = await hre.web3.eth.getAccounts();
+    const contractFactory = await hre.ethers.getContractFactory("TestNFT");
+    const ABI = JSON.parse(contractFactory.interface.formatJson());
+    const contract = new hre.web3.eth.Contract(ABI, replace_contractAddress);
+    const replace_functionArgs = "0x8A1257870E6Ec8F382F0e3ff6c621BFFB0B764e5"; // Recipient address
+    console.log("Minting 1 token...");
+    try {
+        const transaction = await contract.methods.safeMint(replace_functionArgs).send({ from });
+        console.log("Transaction submitted! Transaction details:", transaction);
+        // Display success message with recipient address
+        console.log(`Transaction completed successfully! ✅ Non-Fungible Token minted to ${replace_functionArgs}`);
+        console.log("Transaction hash:", transaction.transactionHash);
+    } catch (error) {
+        console.error(`Transaction failed! Could not mint NFT.`);
+        console.error(error);
+    }
+}
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
